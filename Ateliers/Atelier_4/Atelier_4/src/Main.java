3ee4dab6e1924c7b60df52f6f17e5e8580060cfe
LaurieGuan <boucherlaurie0@gmail.com>
2025-02-09 01:00:33 -0500

    Ajout (Main) - Début implementation de la loop principale du jeu

--- Ateliers/Atelier_4/Atelier_4/src/Main.java
+++ Ateliers/Atelier_4/Atelier_4/src/Main.java
@@ -1,8 +1,6 @@
+import lib.Characters.*;
 import lib.utils.Modes;
-import lib.Characters.Fighter;
-import lib.Characters.Mage;
-import lib.Characters.Cleric;
-import lib.Characters.Entity;
+import lib.utils.Tri;
 
 
 public class Main {
@@ -10,9 +8,13 @@ public class Main {
     {
         Mage pierre = new Mage("Pierre");
         Entity rejean = new Fighter("Rejean");
+        Entity guetan = new Cleric("Guetan");
+        Monster gruk = new Monster("Gruk");
+        Monster knoush = new Monster("Knoush");
+        Monster Darkrim = new Monster("Darkrim", Modes.status.FRIENDLY);
 
-
-        while (Entity.getNombreEntites() > 0) {
+        Entity.lancerCombat();
+        while (Entity.getNombreJoueurs() > 0 && Entity.getNombreMonstres() > 0) {
             Entity.jouerManche();
         }
 
